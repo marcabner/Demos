ae230368323cee4a314d1c43d92eff6c29a9fc3e
Yair Ramírez E <marcabner@gmail.com>
2022-08-12 10:42:57 -0500

    Update Country.java
    Se agregan más issues para probar Sonar Cloud

--- TestingTipsTricks/src/com/pruebas/Country.java
+++ TestingTipsTricks/src/com/pruebas/Country.java
@@ -1,6 +1,9 @@
 package com.pruebas;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class Country {
@@ -10,20 +13,34 @@ public class Country {
     private String url;
 
     private static List<String> listPersons = null;
+    
+    public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    private String filePath = sdf.format(new Date()) + ".xlsx";
+    
+    private static final HashMap<String, String> map_identity = new HashMap();
 
     public static List<String> getInstance()
     {
+    	map_identity.put("es.business.com", "1234567");
+    	map_identity.put("es.business.com", "2345678");
+    	map_identity.put("es.business.com", "3456789");
+    	
         if(listPersons == null)
         {
-        	listPersons = new ArrayList<String>();
+        	listPersons = new ArrayList<>();
         }
+        try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException e) {
+			listPersons = null;
+		}
         return listPersons;
     }
     
     public Country(String code, String name, String url) {
         this.code = code;
         this.name = name;
-        this.url = url;
+        this.url = url + filePath;
     }
 
     public String getCode() {
